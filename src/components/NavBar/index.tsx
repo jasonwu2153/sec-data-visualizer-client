87c78fafef7c09e5d60b1aa389eba23ca1b48fad
Jason Wu <jason.wu2153@gmail.com>
2020-12-09 00:41:54 -0500

    styled title text

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -5,7 +5,7 @@ import { lightGray } from '../../utils/colors';
 const NavBar = () => {
     return (
         <div style={styles.wrapper}>
-            <p>SEC Data Visualizer</p>
+            <p style={styles.titleText}>SEC Data Visualizer</p>
         </div>
     );
 };
@@ -14,8 +14,11 @@ const styles = {
     wrapper: {
         background: lightGray,
         width: '100%',
-        padding: 3,
+        padding: '10px 30px 10px 30px',
         zIndex: 2
+    } as CSSProperties,
+    titleText: {
+        fontWeight: 600
     } as CSSProperties
 };
 
