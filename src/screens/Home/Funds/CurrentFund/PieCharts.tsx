5a711e1bbd49e3790a648f12782faf77b6f09938
Jason Wu <jason.wu2153@gmail.com>
2020-12-09 19:29:38 -0500

    adjusted formatting

--- src/screens/Home/Funds/CurrentFund/PieCharts.tsx
+++ src/screens/Home/Funds/CurrentFund/PieCharts.tsx
@@ -19,44 +19,42 @@ const PieCharts = (props: Props) => {
 
     return (
         <div style={styles.wrapper}>
-            <div style={styles.chartWrapper}>
-                <div style={styles.labelWrapper}>
-                    <p style={styles.label}>
+            <div style={styles.graphContainer}>
+                <div style={styles.graphTitleContainer}>
+                    <p style={styles.graphTitle}>
                         Position Distribution by Value in USD
                     </p>
                 </div>
-                <div style={styles.graphContainer}>
-                    <Doughnut
-                        data={{
-                            datasets: [
-                                {
-                                    data: data.map(prop('value')),
-                                    backgroundColor: data.map((_, i: number) =>
-                                        i < 9 ? blue[9 - i] : blue[1]
-                                    )
-                                }
-                            ],
-                            labels: data.map(prop('name'))
-                        }}
-                        height={600}
-                        width={600}
-                        options={{
-                            aspectRatio: 1,
-                            cutoutPercentage: 80,
-                            legend: { display: false },
-                            tooltips: {
-                                callbacks: {
-                                    label: (item: any) => {
-                                        const entry = data[item.index];
-                                        return ` ${
-                                            entry.name
-                                        }: $${numberWithCommas(entry.value)}`;
-                                    }
+                <Doughnut
+                    data={{
+                        datasets: [
+                            {
+                                data: data.map(prop('value')),
+                                backgroundColor: data.map((_, i: number) =>
+                                    i < 9 ? blue[9 - i] : blue[1]
+                                )
+                            }
+                        ],
+                        labels: data.map(prop('name'))
+                    }}
+                    height={460}
+                    width={460}
+                    options={{
+                        aspectRatio: 1,
+                        cutoutPercentage: 80,
+                        legend: { display: false },
+                        tooltips: {
+                            callbacks: {
+                                label: (item: any) => {
+                                    const entry = data[item.index];
+                                    return ` ${entry.name}: $${numberWithCommas(
+                                        entry.value
+                                    )}`;
                                 }
                             }
-                        }}
-                    />
-                </div>
+                        }
+                    }}
+                />
             </div>
         </div>
     );
@@ -69,24 +67,21 @@ const styles = {
         flexDirection: 'column',
         paddingBottom: 120
     } as CSSProperties,
-    chartWrapper: {
-        flex: 1,
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'row',
-        padding: 60
-    } as CSSProperties,
-    labelWrapper: {
-        marginRight: 60,
-        maxWidth: 200
-    } as CSSProperties,
-    label: {
-        fontSize: 22
-    } as CSSProperties,
     graphContainer: {
-        maxWidth: 600
+        maxWidth: 460,
+        position: 'relative'
+    } as CSSProperties,
+    graphTitleContainer: {
+        maxWidth: 240,
+        position: 'absolute',
+        textAlign: 'center',
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -50%)'
+    } as CSSProperties,
+    graphTitle: {
+        margin: 0,
+        fontSize: 20
     } as CSSProperties
 };
 
