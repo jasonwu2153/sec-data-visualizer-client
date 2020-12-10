a629746461c88f91088ae010421f6a7c0c6368a2
Jason Wu <jason.wu2153@gmail.com>
2020-12-10 18:20:02 -0500

    added holdings data

--- src/screens/Home/Holdings/TopHoldingsDetails.tsx
+++ src/screens/Home/Holdings/TopHoldingsDetails.tsx
@@ -1,3 +1,4 @@
+import { blue } from '@ant-design/colors';
 import React, { CSSProperties } from 'react';
 
 import { PopularHoldingData } from '../../../utils/types';
@@ -7,13 +8,35 @@ interface Props {
 }
 
 const TopHoldingsDetails = (props: Props) => {
-    return <div style={styles.wrapper}></div>;
+    const { data } = props;
+
+    return (
+        <div style={styles.wrapper}>
+            {data.map((entry: PopularHoldingData, i: number) => (
+                <div style={styles.cardContainer} key={i}>
+                    <p style={styles.textStyle}>{entry.name}</p>
+                    <p style={styles.textStyle}>{entry.title}</p>
+                    <p style={styles.textStyle}>LEI: {entry.lei}</p>
+                    <p style={styles.textStyle}>
+                        Held By {entry.held_by} Funds
+                    </p>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 const styles = {
     wrapper: {
         width: '100%',
         marginBottom: 30
+    } as CSSProperties,
+    cardContainer: {
+        background: blue[4],
+        margin: 5
+    } as CSSProperties,
+    textStyle: {
+        margin: 0
     } as CSSProperties
 };
 
